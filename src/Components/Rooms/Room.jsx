1ad576884ea7b11fd369c969918069f920b13ba4
AbdullahWB <abdullah917828@gmail.com>
2023-06-04 22:30:02 +0600

    make card dynamic

---
+++ src/Components/Rooms/Room.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import Container from '../Shared/Container';
+import Card from './Card';
+import Loader from '../Shared/Loader';
+
+const Room = () => {
+    const [rooms, setRooms] = useState([])
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        fetch("./Rooms.json")
+            .then(res => res.json())
+            .then(data => {
+                setRooms(data)
+                setLoading(false)
+            })
+            .catch(err => console.error(err))
+    }, [])
+
+    if (loading) {
+        return <Loader />
+    }
+
+    return (
+        <Container>
+            <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+                {
+                    rooms.map((room, index) => <Card key={index} room={room} />)
+                }
+            </div>
+        </Container>
+    );
+};
+
+export default Room;
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Categories from '../../Components/Categories/Categories';
+import Room from '../../Components/Rooms/Room';
 
 const Home = () => {
     return (
         <div>
             <Categories /> 
+            <Room />
         </div>
     );
 };
